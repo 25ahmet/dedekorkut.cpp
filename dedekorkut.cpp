3b0f238b37cbf02879c05354fde259c1b8c67d41
25ahmet <44473668+25ahmet@users.noreply.github.com>
2018-10-25 18:36:48 +0300

    Add files via upload

---
+++ dedekorkut.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define pii pair<int,int>
+#define rep(i,n) for(int i=0;i<(n);++i)
+#define FOR(i,a,b) for (int i=(a); i<(b); ++i)
+#define INF 0xFFFFFF
+#define endl '\n'
+const int MAXX=1000005;
+const int maxInt = 2147483647;
+using namespace std;
+int main(){
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	ll n=0;
+	while(++n){
+		cout<<n<<"-"<<"Dede Korkut"<<endl;
+	}
+	return 0;
+}
